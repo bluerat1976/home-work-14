7b7d210a38518b0507dbc90aafb5be941c6e1c06
bluerat1976 <osoblivaia76@mail.ru>
2019-03-18 17:09:34 +0200

    New js learning

---
+++ index.js
@@ -0,0 +1,11 @@
+debugger;
+var x = prompt('Input x');
+var n = prompt('Input n');
+x = parseInt(x);
+n = parseInt(n);
+var result = Math.pow(x, n);
+var div = document.createElement('div');
+div.className = 'main-block';
+div.innerHTML = 'Result of x = ' + x + ' in power of n = ' + n + ' equals ' + result+'.';
+var parentElem = document.body;
+parentElem.appendChild(div);
